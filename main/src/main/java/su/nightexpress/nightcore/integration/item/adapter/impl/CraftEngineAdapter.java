bde601e3cf590e809f0693ef2de33ad26810a2ed
Magic_Bili <124496967+addpromax@users.noreply.github.com>
2025-11-11 21:03:28 +0800

    Add craftengine support

---
+++ main/src/main/java/su/nightexpress/nightcore/integration/item/adapter/impl/CraftEngineAdapter.java
@@ -0,0 +1,45 @@
+package su.nightexpress.nightcore.integration.item.adapter.impl;
+
+import net.momirealms.craftengine.bukkit.api.CraftEngineItems;
+import net.momirealms.craftengine.core.item.CustomItem;
+import net.momirealms.craftengine.core.util.Key;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import su.nightexpress.nightcore.integration.item.data.ItemIdData;
+import su.nightexpress.nightcore.integration.item.adapter.IdentifiableItemAdapter;
+
+public class CraftEngineAdapter extends IdentifiableItemAdapter {
+
+    public CraftEngineAdapter() {
+        super("craftengine");
+    }
+
+    @Override
+    @Nullable
+    public ItemStack createItem(@NotNull String itemId) {
+        Key key = Key.of(itemId);
+        CustomItem<ItemStack> customItem = CraftEngineItems.byId(key);
+        return customItem == null ? null : customItem.build();
+    }
+
+    @Override
+    public boolean canHandle(@NotNull ItemStack itemStack) {
+        return CraftEngineItems.isCustomItem(itemStack);
+    }
+
+    @Override
+    public boolean canHandle(@NotNull ItemIdData data) {
+        Key key = Key.of(data.getItemId());
+        return CraftEngineItems.byId(key) != null;
+    }
+
+    @Override
+    @Nullable
+    public String getItemId(@NotNull ItemStack item) {
+        if (item.getType().isAir()) return null;
+
+        Key key = CraftEngineItems.getCustomItemId(item);
+        return key == null ? null : key.asString();
+    }
+}
